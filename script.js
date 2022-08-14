55a779dd429357bde57f4cb4912cac9eb0a1016b
Ritik-Yadav-16 <111215208+Ritik-Yadav-16@users.noreply.github.com>
2022-08-14 11:05:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+<script>
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navba").style.backgroundColor = "#333";
+    document.getElementById("navbar").style.display = "none";
+  }else {
+    document.getElementById("navba").style.backgroundColor = "#3330";
+    document.getElementById("navbar").style.display = "block";
+  }
+}
+</script>
